beace6cbb60f1978b0272b8028ee3db22281e8e0
xiaoyu888888 <xiaoyu1423477717@163.com>
2018-11-15 14:43:45 +0800

    美食百科接口

--- endulajiao/Application/Home/Conf/config.php
+++ endulajiao/Application/Home/Conf/config.php
@@ -2,7 +2,7 @@
 return array(
 	'DB_TYPE'   => 'mysql', // 数据库类型
 	'DB_HOST'   => '127.0.0.1', // 服务器地址
-	'DB_NAME'   => 'hemao', // 数据库名
+	'DB_NAME'   => 'endu', // 数据库名
 	'DB_USER'   => 'root', // 用户名
 	'DB_PWD'    => 'root', // 密码
 	'DB_PORT'   => 3306, // 端口
@@ -12,10 +12,8 @@ return array(
 	'URL_ROUTER_ON'   => true, //开启路由
 	'URL_ROUTE_RULES' => array( //定义路由规则 
 		
-			//单条消息
-			 'dantiao/:id\d'   => 'Index/dantiao',
 		    //多条消息
-		    'duotiao/xiaoxi' => 'Index/duotiao',
+		    'meishi/xinxi' => 'Index/tuijian',
 
 	),
 
--- endulajiao/Application/Home/Controller/IndexController.class.php
+++ endulajiao/Application/Home/Controller/IndexController.class.php
@@ -9,4 +9,27 @@ class IndexController extends Controller {
 
     }
 
+   public function tuijian()
+   {
+
+   		$data = M('meishi')->select();
+   		if($data)
+        {
+
+            $code = 200;
+            $message = 'success';
+
+        }
+        else
+        {
+
+            $code = 404;
+            $message = 'fail';
+        }
+
+        $result = array('code'=>$code, 'message'=>$message,'data'=>$data);
+
+         $this->ajaxReturn($result,'JSONP');
+   }
+   
 }
